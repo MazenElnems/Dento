dbc283c5ba9fe8f6df6d61dac96fb57a2ee4b927
Abdelrahman-development <abdosaber2909@gmail.com>
2026-04-25 14:30:11 +0300

    update contact and gallery pages and routes

--- frontend/dentalclinic-main/src/app/app.routes.ts
+++ frontend/dentalclinic-main/src/app/app.routes.ts
@@ -6,9 +6,12 @@ import { Home } from './home/home';
 import { Services } from './services/services';
 
 export const routes: Routes = [
-  { path: '', component: Home },
+  { path: '' , redirectTo:"home" , pathMatch:'full' , title:"Home"},
+  { path:"home",component:Home, title:"Home"},
   { path: 'services', component: Services },
   { path: 'about', component: About },
   { path: 'gallery', component: Gallery },
-  { path: 'contact', component: Contact },
+  { path: 'contact', component: Contact ,children:[
+    { path: "home" , redirectTo:"home" , pathMatch:'full' , title:"Home"},
+  ]},
 ];

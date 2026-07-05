ef994056c89ec84150925a7ec167b3d59a33f294
Mazen Elnems <mazenelnems22@gmail.com>
2026-07-05 15:13:51 +0300

    Add data annotations and required properties to DTOs
    Added [Required] attributes and init-only required properties to Email, Code, and UserId in SendVerificationCodeRequestDto and VerifyEmailRequestDto for improved validation.

--- backend/src/DTOs/SendVerificationCodeRequestDto.cs
+++ backend/src/DTOs/SendVerificationCodeRequestDto.cs
@@ -1,6 +1,9 @@
-﻿namespace Dento.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dento.DTOs;
 
 public class SendVerificationCodeRequestDto
 {
+    [Required]
     public required string Email { get; init; }
 }
--- backend/src/DTOs/VerifyEmailRequestDto.cs
+++ backend/src/DTOs/VerifyEmailRequestDto.cs
@@ -1,7 +1,11 @@
-﻿namespace Dento.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dento.DTOs;
 
 public class VerifyEmailRequestDto
 {
+    [Required]
     public required string Code { get; init; } 
+    [Required]
     public required string UserId { get; init; }
 }
